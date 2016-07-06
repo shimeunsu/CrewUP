0da438dbe5f5ee12c550d03b6ecc55aef36284c2
Tuochaolong Zhang <ztuochao@uwo.ca>
2016-07-06 11:30:09 -0400

    I dont know it looks same

--- js/crewup.js
+++ js/crewup.js
@@ -4,14 +4,14 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery to collapse the navbar on scroll
-function collapseNavbar() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-}
+ // jQuery to collapse the navbar on scroll
+ function collapseNavbar() {
+     if ($(".navbar").offset().top > 50) {
+         $(".navbar-fixed-top").addClass("top-nav-collapse");
+     } else {
+         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+     }
+ }
 
 $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
